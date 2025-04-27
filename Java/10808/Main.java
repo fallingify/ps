11b3022e620e7b5323850fa064878ff9308a6f1f
fallingify <gnswldl000@gmail.com>
2025-04-27 15:47:05 +0900

    10808

---
+++ Java/10808/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int[] arr = new int[26];  //알파벳 개수를 저장하는 배열
+        String s = br.readLine();  //단어 입력받기
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            arr[c - 97]++;  //개수 1씩 늘려주기
+        }
+        for (int i = 0; i < 26; i++) {
+            bw.write(String.valueOf(arr[i])); 
+            bw.append(' ');
+        }
+        bw.append('\n');
+        bw.flush();
+        
+    
+    }
+}
