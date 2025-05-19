a058ddc3017c8be36a18faa7947c991797598c11
fallingify <gnswldl000@gmail.com>
2025-05-19 23:29:35 +0900

    2178

---
+++ Java/2178/Main.java
@@ -0,0 +1,59 @@
+import java.io.*; 
+import java.util.*; 
+
+
+public class Main {
+    static int[][] map;
+    static boolean[][] visited;
+    static int n, m;
+    static int[] dx = {-1, 1, 0, 0}; 
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(bfs(0, 0));
+
+    }
+
+    public static int bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            int[] pos = queue.poll();
+            int curX = pos[0];
+            int curY = pos[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = curX + dx[i];
+                int ny = curY + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (!visited[nx][ny] && map[nx][ny] == 1) {
+                        queue.add(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                        map[nx][ny] = map[curX][curY] + 1; 
+                    }
+                }
+            }
+        }
+
+        return map[n - 1][m - 1];
+    }
+    
+}
