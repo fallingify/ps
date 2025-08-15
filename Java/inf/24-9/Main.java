46ad191838c914c5b325894698cca82bcd5c35a0
fallingify <gnswldl000@gmail.com>
2025-08-15 10:39:24 +0900

    250815(2)

--- Java/inf/24-9/Main.java
+++ Java/inf/24-9/Main.java
@@ -2,10 +2,27 @@ import java.util.*;
 
 class Main {
     public int solution(int num, int[][] arr) {
-        int answer; 
-
+        int answer = Integer.MIN_VALUE; 
+        int sum_row = 0, sum_col = 0; 
         
+        for(int i=0; i<num; i++) {
+            sum_row = sum_col = 0; 
+            for(int j=0; j<num; j++) {
+                sum_row += arr[i][j]; 
+                sum_col += arr[j][i]; 
+            }
+            answer = Math.max(answer, sum_row); 
+            answer = Math.max(answer, sum_col); 
+        }
+       
+        sum_row = sum_col = 0; 
+        for(int i=0; i<num; i++) {
+            sum_row += arr[i][i]; 
+            sum_col += arr[i][num-i-1]; 
+        }
 
+        answer = Math.max(answer, sum_row); 
+        answer = Math.max(answer, sum_col); 
 
         return answer; 
 
@@ -23,7 +40,7 @@ class Main {
         }
 
 
-        T.solution(n, metrics); 
+        System.out.println(T.solution(n, metrics)); 
 
 
     }
