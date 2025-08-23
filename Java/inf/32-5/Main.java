4348ec7ce635ab92b6764b8be2f746e1db376a6d
fallingify <gnswldl000@gmail.com>
2025-08-23 22:09:59 +0900

    250823

---
+++ Java/inf/32-5/Main.java
@@ -0,0 +1,35 @@
+import java.util.*; 
+//투 포인터 알고리즘 : 2 자연수의 합
+class Main{
+    public int solution(int n) {
+        int answer = 0;     
+        int lt = 0; 
+        int sum = 0; 
+        int m = n/2 + 1; 
+        int[] arr = new int[m];
+
+        
+        for(int i=0; i<m; i++) {
+            arr[i] = i + 1; 
+        }
+        
+        for(int rt =0; rt < m; rt++) {
+            sum += arr[rt]; 
+            if(sum == n) answer++; 
+            while(sum >= n) {
+                sum -= arr[lt++]; 
+                if(sum == n) answer++; 
+            }
+        }
+
+
+        return answer; 
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main(); 
+        Scanner kb = new Scanner(System.in); 
+        int n = kb.nextInt(); 
+        System.out.println(T.solution(n)); 
+    }
+}
