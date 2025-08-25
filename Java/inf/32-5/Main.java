a5e7dae5ccf685f0f4da2acca077016c1acddf07
fallingify <gnswldl000@gmail.com>
2025-08-25 11:20:56 +0900

    250825(1)review

--- Java/inf/32-5/Main.java
+++ Java/inf/32-5/Main.java
@@ -1,19 +1,15 @@
 import java.util.*; 
-//투 포인터 알고리즘 : 2 자연수의 합
+//투 포인터 알고리즘 : 2 자연수의 합(리뷰)
 class Main{
     public int solution(int n) {
-        int answer = 0;     
-        int lt = 0; 
-        int sum = 0; 
-        int m = n/2 + 1; 
-        int[] arr = new int[m];
+        int answer = 0, lt = 0; 
+        int[] arr = new int[n]; 
 
+        int sum = 0; 
+        int m = n / 2 + 1; 
+        for(int i=0; i<m; i++) arr[i] = i + 1; 
         
-        for(int i=0; i<m; i++) {
-            arr[i] = i + 1; 
-        }
-        
-        for(int rt =0; rt < m; rt++) {
+        for(int rt =0; rt<m; rt++) {
             sum += arr[rt]; 
             if(sum == n) answer++; 
             while(sum >= n) {
@@ -21,15 +17,13 @@ class Main{
                 if(sum == n) answer++; 
             }
         }
-
-
-        return answer; 
+        return answer;
     }
-
     public static void main(String[] args) {
         Main T = new Main(); 
         Scanner kb = new Scanner(System.in); 
         int n = kb.nextInt(); 
+        
         System.out.println(T.solution(n)); 
     }
 }
